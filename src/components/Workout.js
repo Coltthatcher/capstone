97b6ffd97d455514beb783146c802c30d6e24539
Colt Thatcher <colt.thatcher303@gmail.com>
2022-02-19 15:02:29 -0800

    Very base site working, temporory props added, no actual functionality yet

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -2,17 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Workout(props){
+  return(
   <React.Fragment>
     <h3>{props.group} - {props.names}</h3>
     <p>{props.description}</p>
     <hr/>
   </React.Fragment>
+  );
 }
 
 
 
-Workout.PropTypes = {
-  names: PropTypes.string,
+Workout.propTypes = {
+  name: PropTypes.string,
   group: PropTypes.string,
   description: PropTypes.string
 };
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -4,19 +4,19 @@ import Workout from "./Workout";
 
 const mainWorkoutList = [
   {
-    name="Workout name",
-    group="Muscle Group",
-    description="Description of Workout"
+    name:"Workout name",
+    group:"Muscle Group",
+    description:"Description of Workout"
   },
   {
-    name="Workout name2",
-    group="Muscle Group2",
-    description="Description of Workout2"
+    name:"Workout name2",
+    group:"Muscle Group2",
+    description:"Description of Workout2"
   },
   {
-    name="Workout name3",
-    group="Muscle Group3",
-    description="Description of Workout3"
+    name:"Workout name3",
+    group:"Muscle Group3",
+    description:"Description of Workout3"
   },
 ];
 
