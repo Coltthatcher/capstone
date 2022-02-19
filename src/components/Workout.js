076cbfadd87c35c6be232218a60127a5d7031e7c
Colt Thatcher <colt.thatcher303@gmail.com>
2022-02-19 14:37:17 -0800

    Add begineer prop.types in workout/workoutlist

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-
-function Workout(){
+function Workout(props){
   <React.Fragment>
-    
+    <h3>{props.group} - {props.names}</h3>
+    <p>{props.description}</p>
+    <hr/>
   </React.Fragment>
 }
 
+
+
+Workout.PropTypes = {
+  names: PropTypes.string,
+  group: PropTypes.string,
+  description: PropTypes.string
+};
 
 export default Workout;
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -4,7 +4,14 @@ import Workout from "./Workout";
 function WorkoutList(){
   return(
     <React.Fragment>
-      
+      <Workout
+        group="Muscle Group"
+        name="Workout name"
+        description="Description of Workout" />
+      <Workout
+        group="Muscle Group2"
+        name="Workout name2"
+        description="Description of Workout2" />
     </React.Fragment>
   );
 }
