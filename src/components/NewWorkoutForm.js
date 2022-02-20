1cf4160a097f63840309a0f449bd331a2d110250
Colt Thatcher <colt.thatcher303@gmail.com>
2022-02-19 16:42:18 -0800

    Add in uuid libray for starting newworkoutform

--- src/components/NewWorkoutForm.js
+++ src/components/NewWorkoutForm.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { v4 } from  "uuid";
 
 function NewWorkoutForm(props){
   return (
     <React.Fragment>
-      
+      <form onSubmit={handleNewWorkoutFormSubmission}>
+        <input
+          type="text"
+          name="name"
+          placeholder="workout name"/>
+        <input
+          type="text"
+          name="group"
+          placeholder="muscle group"/>
+        <input
+          type="text"
+          name="description"
+          placeholder="description of work out"/>
+      </form>
     </React.Fragment>
   )
 }
