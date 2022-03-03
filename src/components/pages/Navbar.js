bc7133cd3c5044dfa20b8e86a9692e34a6a958e9
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-03 10:13:54 -0800

    Page working, set styles for buttons i wish to implement

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -2,12 +2,24 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdDirectionsRun } from "react-icons/md";
 import { FaBars, FaTimes } from 'react-icons/fa';
-
+import { Button } from '../Button';
 
 function Navbar() {
   const [click, setClick] = useState(false)
 
+  const [button, setButton] = useState(true)
+
   const handleClick = () => setClick(!click)
+
+  const closeMobileMenu = () => setClick(false)
+
+  const showButton = () => {
+    if(window.innerWidth <= 960 ) {
+      setButton(false)
+    } else {
+      setButton(true);
+    }
+  }
 
   return (
     <React.Fragment>
