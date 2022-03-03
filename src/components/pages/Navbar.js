7607243167c92134672ed9aae48ce90e533e7646
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-03 08:58:00 -0800

    downloading dependices icons and react router dom

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <React.Fragment>
+      <div className='navbar'>
+        <div className='navbar-container container'>
+          <Link to='/' className='navbar-logo'>
+            Goal
+          </Link>
+          <div className='menu-icon'>
+
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 
