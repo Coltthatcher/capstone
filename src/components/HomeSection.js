19085a923276931717d1376cc0694d2a75b6a1db
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-05 12:58:23 -0800

    add code to home page, toggle ability between what screen is shown

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -4,10 +4,23 @@ import {Link} from 'react-router-dom'
 import './HomeSection.css'
 
 
-function HomeSection() {
+function HomeSection({
+  lightbg, topLine, lightText, lightTextDesc, headline, description, buttonLabel, img, alt, imgStart
+}) {
   return (
     <div className={lightbg ? 'home__home-section': 'home__home-section darkBg'}>
-      
+      <div className='container'>
+        <div className='row home__home-section' style={{display: 'flex', flexDirection: imgStart === 'start' ? 'row-reverse' : 'row' }}
+        >
+          <div className='col'>
+            <div className='home__home-text-wrapper'>
+              <div className='top-line'>{topLine}</div>
+              <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
