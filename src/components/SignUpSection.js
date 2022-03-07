9ad608f03022665e9a2e6c84ba5841da61fc680b
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-07 15:10:17 -0800

    adding in footer component

---
+++ src/components/SignUpSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignUpSection() {
+  return (
+    <div>SignUpSection</div>
+  )
+}
+
+export default SignUpSection
---
+++ src/components/pages/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "../../Button";
+import { Link } from "react-router-dom";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import { MdDirectionsRun } from "react-icons/md";
+
+
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
