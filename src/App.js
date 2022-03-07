39ede00941e385b51755ad4c5773c17b4777f77c
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-07 15:39:56 -0800

    messing with footer css, cant get footer to redner proporley

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/pages/Footer/Footer';
 import Home from './components/pages/HomePage/Home';
 import SignUp from './components/pages/HomePage/SignUp';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' exact element={<Home/>} />
         <Route path='/sign-up' exact element= {<SignUp/>} ></Route>
       </Routes>
+      <Footer />
     </Router>
   );
 }
