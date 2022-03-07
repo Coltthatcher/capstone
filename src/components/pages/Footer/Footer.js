b3f8bb48ae367c2685251194c190554527308c38
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-07 15:20:58 -0800

    continue working on footer

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -14,7 +14,27 @@ import { MdDirectionsRun } from "react-icons/md";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-heading">
+          Join us and get on your goals
+        </p>
+        <p className='footer-subscription-text'>
+          unsubscribe at anytime
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              className="footer-input"
+              name='email'
+              type='email'
+              placeholder="Your Email"
+            />
+            <Button buttonStyle='btn--outline'>Subscribe</Button>
+          </form>
+        </div>
+      </section>
+    </div>
   )
 }
 
