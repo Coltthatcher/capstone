e78909c0e9ee659ff9d3a7ab5ef58820541fe651
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-08 11:07:32 -0800

    update css all over page

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -43,9 +43,26 @@ function Pricing() {
                   <li>Thousands of workouts</li>
                   <li>Create Personalized Plans</li>
                   <li>Integrated Music match</li>
-                  <li>Trainers Utilization</li>
                 </ul>
-                <Button buttonSize='btn--wide' buttonColor='blue'>
+                <Button buttonSize='btn--wide' buttonColor='primary'>
+                  Choose Premium
+                </Button>
+              </div>
+            </Link>
+            <Link to='/sign-up' className='pricing-container-sheet'>
+              <div className='price_container-info'>
+                <div className='icon'>
+                  <BsFlower3 />
+                </div>
+                <h3>Premium</h3>
+                <h4>$15.99</h4>
+                <p>per month</p>
+                <ul className='container-tiers'>
+                  <li>Thousands of workouts</li>
+                  <li>Create Personalized Plans</li>
+                  <li>Integrated Music match</li>
+                </ul>
+                <Button buttonSize='btn--wide' buttonColor='primary'>
                   Choose Premium
                 </Button>
               </div>
