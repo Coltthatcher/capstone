4d1e7bbc787c4b67add9e11e61c6d5c93cbfcb1b
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-08 11:38:50 -0800

    tie up loose routing ends

---
+++ src/components/pages/Subscription/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne={
+  lightBg:false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Workout planner',
+  headline: 'Unlimited Workouts to help your goal',
+  description: 'Get access to thousands of workouts. Mix match and plan new or known techniques to a personalized list allowing complete user creativity to accomplish your goals',
+  buttonLabel: "Get on your Goals",
+  imgStart: '',
+  img: "images/zen.icon.png",
+  alt: 'flower',
+}
+
+export const homeObjTwo={
+  lightBg:false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Choose from Top tier trainers',
+  headline: 'Our Trainers are World Class',
+  description: 'Our personalized trainers, are top trained and from around the globe. They can provide personalized workout plans based on you!',
+  buttonLabel: "Meet the Trainers",
+  imgStart: '',
+  img: "images/fitness-runner-hi.png",
+  alt: 'Runner',
+}
+
+export const homeObjThree={
+  lightBg:false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Integrated Soundcloud Compatibility',
+  headline: 'Workouts the way you like to hear them',
+  description: 'Soundcloud integration allows for seamless addition of music to your plans, personalization has never sounded better',
+  buttonLabel: "Try It Out",
+  imgStart: '',
+  img: "images/music.png",
+  alt: 'flower',
+}
+
+export const homeObjFour={
+  lightBg:false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Join Us',
+  headline: 'We Want You To be Part Of The Fastest Growing Personalized Workout Base',
+  description: 'Join the team set a new standard of goals',
+  buttonLabel: "Join us",
+  imgStart: '',
+  img: "images/zen.icon.png",
+  alt: 'flower',
+}
