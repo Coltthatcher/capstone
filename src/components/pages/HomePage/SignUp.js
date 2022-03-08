bc40edc7f778a18cc0e59b586865eadd1bca9a1e
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-08 13:48:51 -0800

    editing css of signup

--- src/components/pages/HomePage/SignUp.js
+++ src/components/pages/HomePage/SignUp.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import HomeSection from '../../HomeSection'
-import {homeObjOne, homeObjThree, homeObjTwo, homeObjFour} from './Data'
+import { Button } from "../../Button";
+
+
 
 function SignUp() {
   return (
     <React.Fragment>
-      <HomeSection {...homeObjOne} />
+        <div className='sign-up-container'>
+      <section className='sign-up-subscription'>
+        <p className='sign-up-subscription-heading'>
+          Subscribe and join GOAL
+        </p>
+        <p className='sign-up-subscription-text'>
+          unsubscribe at any time
+        </p>
+        <div className='sign-up-areas'>
+          <form>
+            <input
+              className='sign-up-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+              placeholder='password'
+            />
+            <Button buttonStyle='btn--outline'>Subscribe</Button>
+          </form>
+        </div>
+        </section>
+        </div>
+      
 
     </React.Fragment>
   )
