60a9cd4e257c36ad422741e15bf444c979b468d2
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-09 16:51:12 -0800

    messing with css of workout page

--- src/components/pages/HomePage/WorkoutPlans.js
+++ src/components/pages/HomePage/WorkoutPlans.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import React from 'react'
 import { db } from "./firebase-config";
 import { collection, getDocs } from "firebase/firestore";
+import "./WorkoutPlans.css"
 
 
 const WorkoutPlans = () => {
@@ -20,21 +21,30 @@ const WorkoutPlans = () => {
     <div className='workout_section'>
       <div className='workout_wrapper'>
         <div className='workout_container'>
-          <div className='col'>
+          <div className='columns'>
             <div className="workouts">
               {workouts.map((workouts) => {
                 return (
                   <div>
                     {" "}
+                  <div className="col">
                   <h1>Name: {workouts.name}</h1>
-                  <h1>Muscle-Group: {workouts.group}</h1>
+                  </div>
+                  <div className="col2">
+                    <h1>Muscle-Group: {workouts.group}</h1>
+                  </div>
+                  <div className="col3">
+                    <h1>Description: {workouts.description}</h1>
+                  </div>
+                  
                   </div>
                 )
               })}
             </div>
-            </div>
           </div>
+          
         </div>
+      </div>
     </div>
     </React.Fragment>
   )
