adcc4043520a982bfa47ccfae200c8fd58809dd8
Colt Thatcher <colt.thatcher303@gmail.com>
2022-03-20 14:02:52 -0700

    did some research on sql, continued to think of layout of personal page i would liek

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import'./Personal.css'
+import { Link } from 'react-router-dom';
 
 function Personal() {
   return (
- 
-  <div className='wrapper-personal'>
-  <div className='Tittle'> Personal Hub
-  </div>
-      <div className='add-column'>
-          <h2>
-            Add workouts to plans
-          </h2>
-      </div>
+  <div className='personal_section'>
+    <div className='wrapper_personal'>
+      <h1 className='personal_heading'>Personal Hub</h1>
+      <Link to='/workoutplans' className="workout-plans-sheet">
+        <div className='personal-plans'></div>
+      </Link>
     </div>
+  </div>
+
   
   )
 }
